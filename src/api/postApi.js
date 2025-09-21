11d24900f3d9338dc8e7f982b1ba459347ee175a
kimsinok0714 <javaenjoy0714@gmail.com>
2025-09-21 12:31:06 +0900

    Update postApi.js

--- src/api/postApi.js
+++ src/api/postApi.js
@@ -5,8 +5,6 @@ export const API_SERVER_HOST = "http://localhost:8080";
 const prefix = `${API_SERVER_HOST}/api/v1`;  // Template Literal
 
 
-
-
 export const getArticle = async (id) => {
 
     console.log('게시글 번호 : ', id);
@@ -43,7 +41,6 @@ export const postArticle = async (article) => {
 }
 
 
-
 export const putArticle = async (article) => {
 
     const result = await axios.put(`${prefix}/posts/${article.no}`, article);
@@ -52,14 +49,16 @@ export const putArticle = async (article) => {
 }
 
 
-
+// 게시글 목록 조회 (페이징 + 검색)
 export const getSearchPostList = async ({ page, size, keyfield, keyword }) => {
-    console.log('--------------------------- getSearchPostList')
     console.log('page : ', page);
     console.log('size : ', size);
     console.log('keyfield : ', keyfield);
     console.log('keyword : ', keyword);
 
+    // axios가 자동으로 쿼리 스트링을 처리
+    // axios가 URL 인코딩을 자동 처리
+    // URL 인코딩 : URL에 특수문자나 한글 등이 포함되면 웹에서 안전하게 전송하기 위해 인코딩이 필요합니다.
     const result = await axios.get(`${prefix}/posts`, { params: { page, size, keyfield, keyword }});
 
     return result.data;
